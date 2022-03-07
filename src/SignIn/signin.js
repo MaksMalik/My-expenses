bba1a109cc8f8b386f15f6180b9d58449d508b3e
MaksMalik <maksymilian.malik@gmail.com>
2022-03-07 23:36:01 +0100

    delete const

--- src/SignIn/signin.js
+++ src/SignIn/signin.js
@@ -22,7 +22,6 @@ onAuthStateChanged(authentication, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
     // ...
   } else {
     // User is signed out
