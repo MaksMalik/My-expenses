c200f471b7ae56aafa273ac8bbd3b0cac385c910
MaksMalik <maksymilian.malik@gmail.com>
2022-03-08 18:44:43 +0100

    Login in/ logout completed

--- src/SignIn/afterlogin.js
+++ src/SignIn/afterlogin.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
+const AfterLogin = ({data, LogOut}) => {
+  return (
+    <Container  maxWidth="xs">
+      <h1 style={{textAlign: "center", paddingTop: "20px", paddingBottom: "20px"}}>Welcome {data?.displayName}</h1>
+      <Button
+        fullWidth
+        variant="contained"
+        sx={{ mt: 0, mb: 2 }}
+        onClick={LogOut}>Logout
+      </Button>
+    </Container>
+  )
+}
+
+
+export default AfterLogin
--- src/SignIn/signin.js
+++ src/SignIn/signin.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -13,30 +11,48 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useState} from "react";
-import {GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import {GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import {authentication} from "../Firebase/firebase";
+import AfterLogin from "./afterlogin";
 
 export default function SignIn() {
 
+  const provider = new GoogleAuthProvider()
   const theme = createTheme();
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [realUser, setRealUser] = useState({})
+
   const [failedLogIn, setFailedLogIn] = useState(false)
+  const [failedRegister, setFailedRegister] = useState(false)
+  const [succeedRegistered, setSucceedRegistered] = useState(false)
+
+
+
   const [loginEmail, setLoginEmail] = useState("")
   const [loginPassword, setLoginPassword] = useState("")
+  const [registerEmail, setRegisterEmail] = useState("")
+  const [registerPassword, setRegisterPassword] = useState("")
+
+  const [isRegistered, setIsRegistered] = useState(false)
 
   onAuthStateChanged(authentication, (currentUser) => {
     setRealUser(currentUser)
   })
 
-  const handleSubmit = async (event) => {
+  const MoveToLogin = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    setLoginEmail(data.get('email'))
-    setLoginPassword(data.get('password'))
+    setIsRegistered(true)
+  }
 
+  const MoveToRegister = (event) => {
+    event.preventDefault();
+    setIsRegistered(false)
+    setFailedLogIn(false)
+  }
+
+  const handleSubmitLogin = async (event) => {
+    event.preventDefault();
     try {
       const user = await signInWithEmailAndPassword (
         authentication,
@@ -44,14 +60,34 @@ export default function SignIn() {
         loginPassword)
       .then(() => setIsLoggedIn(true))
       .then(() => setFailedLogIn(false))
-
       console.log(user)
     } catch (error) {
       setFailedLogIn(true)
     }
   };
 
-  const provider = new GoogleAuthProvider()
+  const handleSubmitRegister = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await createUserWithEmailAndPassword (
+        authentication,
+        registerEmail,
+        registerPassword)
+      console.log(user)
+      setFailedRegister(false)
+      setSucceedRegistered(true)
+      setTimeout(() => {
+        setIsRegistered(true)
+      }, 3000)
+      setTimeout(() => {
+        setSucceedRegistered(false)
+      }, 3000)
+
+
+    } catch (error) {
+      setFailedRegister(true)
+    }
+  };
 
   const SignWithFirebase = () => {
     signInWithPopup(authentication, provider)
@@ -61,7 +97,6 @@ export default function SignIn() {
     })
     .then(() => setIsLoggedIn(true))
     .then(() => setFailedLogIn(false))
-
     .catch(error => {
       console.log(error)
     })
@@ -95,81 +130,138 @@ export default function SignIn() {
               <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
                 <LockOutlinedIcon />
               </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                {failedLogIn && (<div style={{color: 'red'}}>Wrong e-mail or password</div>)}
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign In
-                </Button>
-                <Button
-                  onClick={SignWithFirebase}
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 0, mb: 2 }}
-                >
-                  Sign In with Google
-                </Button>
+              {isRegistered && (
+                <>
+                  <Typography component="h1" variant="h5">
+                    Sign in
+                  </Typography>
+                  <Box component="form" onSubmit={handleSubmitLogin} noValidate sx={{mt: 1}}>
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      autoFocus
+                      onChange={(event) => setLoginEmail(event.target.value)}
 
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link href="#" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Box>
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      onChange={(event) => setLoginPassword(event.target.value)}
+                    />
+                    {failedLogIn && (<div style={{color: 'red'}}>Wrong e-mail or password</div>)}
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{mt: 3, mb: 2}}
+                    >
+                      Sign In
+                    </Button>
+                    <Button
+                      onClick={SignWithFirebase}
+                      fullWidth
+                      variant="contained"
+                      sx={{mt: 0, mb: 2}}
+                    >
+                      Sign In with Google
+                    </Button>
+                    <Grid container>
+                      <Grid item xs>
+                        <Link href="#" variant="body2">
+                          Forgot password?
+                        </Link>
+                      </Grid>
+                      <Grid item>
+                        <Link style={{cursor: "pointer"}} variant="body2" onClick={MoveToRegister}>
+                          {"Don't have an account? Sign Up"}
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </>
+              )}
+
+              {!isRegistered && (
+                <>
+                  <Typography component="h1" variant="h5">
+                    Sign up
+                  </Typography>
+                  <Box component="form" onSubmit={handleSubmitRegister} noValidate sx={{mt: 1}}>
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      autoFocus
+                      onChange={(event) => setRegisterEmail(event.target.value)}
+                    />
+                    <TextField
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      onChange={(event) => setRegisterPassword(event.target.value)}
+
+                    />
+                    {failedRegister && (<div style={{color: 'red'}}>Wrong e-mail or password</div>)}
+                    {succeedRegistered && (<div style={{color: 'green'}}>Successfully registered. Please sign in.</div>)}
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{mt: 3, mb: 2}}
+                    >
+                      Sign up
+                    </Button>
+                    <Button
+                      onClick={SignWithFirebase}
+                      fullWidth
+                      variant="contained"
+                      sx={{mt: 0, mb: 2}}
+                    >
+                      Sign In with Google
+                    </Button>
+                    <Grid container>
+                      <Grid item xs>
+
+                      </Grid>
+                      <Grid item>
+                        <Link style={{cursor: "pointer"}} variant="body2" onClick={MoveToLogin}>
+                          {"Already have an account"}
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </>
+              )}
+
             </Box>
           </Container>
         </ThemeProvider>
       )}
+
+
       {isLoggedIn && (
         <>
-          <Container  maxWidth="xs">
-            <h1 style={{textAlign: "center", paddingTop: "20px", paddingBottom: "20px"}}>Welcome {realUser?.displayName}</h1>
-
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 0, mb: 2 }}
-              onClick={LogOut}>Logout
-            </Button>
-          </Container>
+          <AfterLogin data={realUser} LogOut={LogOut}/>
         </>
       )}
     </>
