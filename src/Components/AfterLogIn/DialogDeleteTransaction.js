7bf7592d4850b14286fb074d4d09278b6f2ea0ea
MaksMalik <maksymilian.malik@gmail.com>
2022-03-14 02:35:01 +0100

    add confirm before delete

--- src/Components/AfterLogIn/DialogDeleteTransaction.js
+++ src/Components/AfterLogIn/DialogDeleteTransaction.js
@@ -33,7 +33,7 @@ const DialogDeleteTransaction = ({ handleCloseDelete, handleDelete, openDelete})
           aria-describedby="alert-dialog-description"
         >
           <DialogTitle style={{backgroundColor:"rgb(255,255,255)", color:"rgb(0,153,189)"}} id="alert-dialog-title">
-            {"Do you really want to delete this transaction?"}
+            {"Do you want to delete this transaction?"}
           </DialogTitle>
           <DialogActions style={{backgroundColor:"rgb(255,255,255)", color:"#0099bd"}}>
             <Button style={{ color:"#0099bd"}} onClick={handleDelete} autoFocus>Delete</Button>
